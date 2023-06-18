48c98c3f3adfbb22c37c1ffd8b7e34ce5c4b5e85
Alex White <alex.white456@gmail.com>
2023-06-18 11:38:42 -0700

    Add pre-commit because our hygiene is terrible :)

--- ncservices/urls.py
+++ ncservices/urls.py
@@ -3,10 +3,10 @@ from allianceauth import urls
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'', include(urls)),
+    re_path(r"", include(urls)),
 ]
 
-handler500 = 'allianceauth.views.Generic500Redirect'
-handler404 = 'allianceauth.views.Generic404Redirect'
-handler403 = 'allianceauth.views.Generic403Redirect'
-handler400 = 'allianceauth.views.Generic400Redirect'
+handler500 = "allianceauth.views.Generic500Redirect"
+handler404 = "allianceauth.views.Generic404Redirect"
+handler403 = "allianceauth.views.Generic403Redirect"
+handler400 = "allianceauth.views.Generic400Redirect"
